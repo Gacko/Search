831e2c447d0f93bf54e4c33d3ef103b794bf9d6c
Marco Ebert <marco_ebert@icloud.com>
2016-05-20 20:43:22 +0200

    Util: Convert RichListenableActionFuture to implicit conversion.

--- app/util/Helpers.scala
+++ app/util/Helpers.scala
@@ -3,6 +3,7 @@ package util
 import org.elasticsearch.action.{ActionListener, ListenableActionFuture}
 
 import scala.concurrent.{Future, Promise}
+import scala.language.implicitConversions
 
 /**
   * Marco Ebert 18.05.16
@@ -10,22 +11,18 @@ import scala.concurrent.{Future, Promise}
 
 object Helpers {
 
-  implicit class RichListenableActionFuture[T](actionFuture: ListenableActionFuture[T]) {
+  implicit def listenableActionFuture2Future[T](actionFuture: ListenableActionFuture[T]): Future[T] = {
+    val promise = Promise[T]()
 
-    def future: Future[T] = {
-      val promise = Promise[T]()
+    actionFuture.addListener(new ActionListener[T] {
 
-      actionFuture.addListener(new ActionListener[T] {
+      override def onFailure(throwable: Throwable): Unit = promise failure throwable
 
-        override def onFailure(throwable: Throwable): Unit = promise failure throwable
+      override def onResponse(response: T): Unit = promise success response
 
-        override def onResponse(response: T): Unit = promise success response
+    })
 
-      })
-
-      promise.future
-    }
-
+    promise.future
   }
 
 }
