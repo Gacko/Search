3d55d6842e457049aca328eb6a7c77482c89f880
Marco Ebert <marco_ebert@icloud.com>
2016-05-21 02:16:26 +0200

    Abstract index service.

---
+++ app/services/AbstractIndexService.scala
@@ -0,0 +1,55 @@
+package services
+
+import org.elasticsearch.client.Client
+import util.Helpers._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * Marco Ebert 20.05.16
+  */
+abstract class AbstractIndexService {
+
+  protected def client: Client
+
+  protected implicit def context: ExecutionContext
+
+  protected def index: String
+
+  protected def settings: String
+
+  /**
+    * Creates the tag index.
+    *
+    * @return
+    */
+  def create: Future[Boolean] = {
+    val request = client.admin().indices().prepareCreate(index)
+    request.setSettings(settings)
+    val response = request.execute()
+    response.map(_.isAcknowledged)
+  }
+
+  /**
+    * Deletes the tag index.
+    *
+    * @return
+    */
+  def delete: Future[Boolean] = {
+    val request = client.admin().indices().prepareDelete(index)
+    val response = request.execute()
+    response.map(_.isAcknowledged)
+  }
+
+  /**
+    * Checks if the tag index exists.
+    *
+    * @return
+    */
+  def exists: Future[Boolean] = {
+    val request = client.admin().indices().prepareExists(index)
+    val response = request.execute()
+    response.map(_.isExists)
+  }
+
+}
