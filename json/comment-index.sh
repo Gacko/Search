d1192eb00cfa5ccc3580921ff93e16123d127302
Marco Ebert <marco_ebert@icloud.com>
2016-05-21 23:59:40 +0200

    JSON: Use correct document for indexing comments.

--- json/comment-index.sh
+++ json/comment-index.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --request PUT --header "Content-Type: application/json" "http://localhost:9000/comments" --data @`dirname $0`/tags.json
+curl --request PUT --header "Content-Type: application/json" "http://localhost:9000/comments" --data @`dirname $0`/comments.json
 echo ""
