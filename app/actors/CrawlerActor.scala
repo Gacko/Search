5378b089758d48c1f6b764853cc02cb704604d1c
Marco Ebert <marco_ebert@icloud.com>
2016-10-01 18:15:53 +0200

    Implement CrawlerActor.

---
+++ app/actors/CrawlerActor.scala
@@ -0,0 +1,56 @@
+package actors
+
+import javax.inject.Inject
+
+import actors.CrawlerActor.Start
+import actors.CrawlerActor.Status
+import actors.CrawlerActor.Stop
+import akka.actor.Actor
+import play.api.Logger
+import services.CrawlerService
+import services.PostService
+
+/**
+  * Marco Ebert 24.09.16
+  */
+object CrawlerActor {
+
+  /**
+    * Actor name.
+    */
+  final val Name = "crawler-actor"
+
+  /**
+    * Start message.
+    */
+  case object Start
+
+  /**
+    * Stop message.
+    */
+  case object Stop
+
+  /**
+    * Status message.
+    */
+  case object Status
+
+}
+
+final class CrawlerActor @Inject()(crawler: CrawlerService, service: PostService) extends Actor {
+
+  /**
+    * Handle crawler actor messages.
+    *
+    * @return Receive.
+    */
+  override def receive: Receive = {
+    case Start =>
+      Logger.info("CrawlerActor::receive: Received start message.")
+    case Stop =>
+      Logger.info("CrawlerActor::receive: Received stop message.")
+    case Status =>
+      Logger.info("CrawlerActor::receive: Received status message.")
+  }
+
+}
