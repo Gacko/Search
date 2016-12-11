faa5c7053797ed87a83603066298dec4d2b86738
Marco Ebert <marco_ebert@icloud.com>
2016-12-11 17:44:12 +0100

    Implement crawler.

--- app/actors/Crawler.scala
+++ app/actors/Crawler.scala
@@ -1,7 +1,14 @@
 package actors
 
+import javax.inject.Inject
+
 import akka.actor.Actor
+import dao.item.ItemDAO
+import models.item.Items
 import play.api.Logger
+
+import scala.util.Failure
+import scala.util.Success
 
 /**
   * Marco Ebert 11.12.16
@@ -23,22 +30,57 @@ object Crawler {
     */
   case object Stop
 
+  /**
+    * Crawl items.
+    *
+    * @param newer Items newer than this.
+    */
+  case class Crawl(newer: Int)
+
 }
 
-final class Crawler extends Actor {
+final class Crawler @Inject()(dao: ItemDAO) extends Actor {
+
+  import context.dispatcher
 
   /**
     * Busy behaviour.
     */
   private def busy: Receive = {
+    case Crawler.Crawl(newer) =>
+      // Find items.
+      dao find Some(newer) onComplete {
+        case Success(Items(_, _, _, items)) =>
+          if (items.nonEmpty) {
+            // Get IDs.
+            val head = items.head.id
+            val last = items.last.id
+
+            Logger info s"Crawler::busy::crawl: $head - $last"
+
+            // Continue crawling.
+            self ! Crawler.Crawl(last)
+          } else {
+            Logger info "Crawler::busy::crawl: No more items."
+            // Stop crawling.
+            self ! Crawler.Crawl(0)
+          }
+        case Failure(exception) =>
+          Logger error s"Crawler::busy::crawl: Failed to fetch items newer than $newer: $exception"
+          // Retry.
+          self ! Crawler.Crawl(newer)
+      }
     // Already crawling.
     case Crawler.Start =>
       Logger info "Crawler::busy::start: Already crawling."
+      // Return failure.
       sender ! false
     // Stop crawling.
     case Crawler.Stop =>
+      // Return to default behaviour.
       context.unbecome()
       Logger info "Crawler::busy::stop: Stopped crawling."
+      // Return success.
       sender ! true
   }
 
@@ -49,11 +91,16 @@ final class Crawler extends Actor {
     // Start crawling.
     case Crawler.Start =>
       Logger info "Crawler::receive::start: Starting crawling."
+      // Become busy.
       context become busy
+      // Start crawling.
+      self ! Crawler.Crawl(0)
+      // Return success.
       sender ! true
     // Not crawling.
     case Crawler.Stop =>
       Logger info "Crawler::receive::stop: Not crawling."
+      // Return failure.
       sender ! false
   }
 
