36395db6450563ff266ae46d46379f1741cd3d79
Marco Ebert <marco_ebert@icloud.com>
2016-12-17 14:42:48 +0100

    Models/Items: Error is a string.

--- app/models/item/Items.scala
+++ app/models/item/Items.scala
@@ -1,13 +1,12 @@
 package models.item
 
-import play.api.libs.json.JsValue
 import play.api.libs.json.Json
 import play.api.libs.json.Reads
 
 /**
   * Marco Ebert 29.06.16
   */
-case class Items(atStart: Boolean, atEnd: Boolean, error: Option[JsValue], items: Seq[Item])
+case class Items(atStart: Boolean, atEnd: Boolean, error: Option[String], items: Seq[Item])
 
 object Items {
 
